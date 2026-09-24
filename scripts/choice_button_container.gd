d0a85bf91aff5d45a21d284bb2192b180523e22a
rynn <120056084+ryrwynn@users.noreply.github.com>
2026-09-24 14:37:12 -0700

    load screen
    basic load screen with themed buttons

--- scripts/choice_button_container.gd
+++ scripts/choice_button_container.gd
@@ -1,4 +1,9 @@
 extends PanelContainer
 
+@export var choice_text : String
+
+func _ready() -> void:
+	$ChoiceButton/MarginContainer/RichTextLabel.text = choice_text
+
 func set_text(text:String) -> void:
 	$ChoiceButton/MarginContainer/RichTextLabel.text = text
